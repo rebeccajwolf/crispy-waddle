dfe44ad90b562b6728ff8aba5c5820c15d5bd9f8
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2024-10-28 14:02:04 +0530

    Update

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ def browserSetup(isMobile: bool = False, proxy: str = None) -> WebDriver:
     options.add_argument('--disable-features=PrivacySandboxSettings4')
     options.add_argument("--disable-search-engine-choice-screen")
     options.add_argument("--disable-http2")
-    options.page_load_strategy = 'eager'
+    # options.page_load_strategy = 'eager'
     if platform.system() == 'Linux':
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
